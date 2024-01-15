a8473df8d3efe98164ccd5f257bc20e693844924
rastemax <maximer819@gmail.com>
2024-01-15 10:40:17 +0100

    fichier +2 uno

--- src/PaireChaineEntier.java
+++ src/PaireChaineEntier.java
@@ -1,3 +1,18 @@
 public class PaireChaineEntier {
 
+    private String chaine;
+    private int entier;
+
+    public PaireChaineEntier(String chaine,int entier){
+        this.chaine=chaine;
+        this.entier=entier;
+    }
+
+    public String getChaine() {
+        return chaine;
+    }
+
+    public int getEntier() {
+        return entier;
+    }
 }
