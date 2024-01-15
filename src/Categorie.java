60e06266c365aaa43549b8965a2fe8b1dee1ddf6
rastemax <maximer819@gmail.com>
2024-01-15 10:52:46 +0100

    fichier uno

--- src/Categorie.java
+++ src/Categorie.java
@@ -26,7 +26,19 @@ public class Categorie {
 
     // initialisation du lexique de la catégorie à partir du contenu d'un fichier texte
     public void initLexique(String nomFichier) {
+        try{
+            FileInputStream file= new FileInputStream(nomFichier);
+            Scanner scanner = new Scanner(file);
 
+            while(scanner.hasNextLine()){
+                String ligne= scanner.nextLine();
+                String id = ligne.substring(3);
+                ligne=scanner.nextLine();
+                String date = ligne.substring(3);
+            }
+        }catch (IOException e){
+            e.printStackTrace();;
+        }
     }
 
 
