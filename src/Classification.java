04a65158e49670d4bf60341add32d3fa7aa866dc
EddyFrc <eddy.francou@gmail.com>
2024-01-17 16:15:53 +0100

    initDico et calculScores

--- src/Classification.java
+++ src/Classification.java
@@ -43,9 +43,11 @@ public class Classification {
         PaireChaineEntier currentPaire;
         ArrayList<PaireChaineEntier> scoreDepeches;
         ArrayList<PaireChaineEntier> bonnesReponses = new ArrayList<>(categories.size());
+
         for (int j = 0; j < categories.size(); j++) {
             bonnesReponses.add(new PaireChaineEntier(categories.get(j).getNom(), 0));
         }
+
         try {
             FileWriter file = new FileWriter(nomFichier);
             for (int i = 0; i < depeches.size(); i++) {
@@ -62,7 +64,6 @@ public class Classification {
                     currentPaire = bonnesReponses.get(UtilitairePaireChaineEntier.indicePourChaine(bonnesReponses, trueCategorie));
                     currentPaire.setEntier(currentPaire.getEntier() + 1);
                 }
-
             }
             
             for (int i = 0; i < bonnesReponses.size(); i++) {
@@ -70,34 +71,69 @@ public class Classification {
             }
             file.write("MOYENNE : " + UtilitairePaireChaineEntier.moyenne(bonnesReponses) + '%');
             file.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static ArrayList<PaireChaineEntier> initDico(ArrayList<Depeche> depeches, String categorie) {
-        ArrayList<PaireChaineEntier> dico = new ArrayList<>();
+        ArrayList<PaireChaineEntier> dicoMots = new ArrayList<>(); // Dictionnaire renvoyé à la fin
+        String currentWord;
+        boolean motAbsent;
         for (int i = 0; i < depeches.size(); i++) {
+            // Parcours de toutes les dépêches
             if (depeches.get(i).getCategorie().equals(categorie)) {
+                // Si la dépêche n°i+1 est dans la catégorie recherchée, on ajoute les mots de cette dépêche suivant s'ils sont déjà dans le dictionnaire ou pas :
                 for (int j = 0; j < depeches.get(i).getMots().size(); j++) {
-
+                    // Parcours de tous les mots de cette dépêche
+                    motAbsent = true;
+                    currentWord = depeches.get(i).getMots().get(j);
+                    for (int k = 0; k < dicoMots.size(); k++) {
+                        // Si un mot est présent à un moment donné, motAbsent prendra la valeur false
+                        if (dicoMots.get(k).getChaine().equals(currentWord)) {
+                            motAbsent = false;
+                        }
+                    }
+                    // Si le mot n'a pas été trouvé, motAbsent vaut true et on peut l'ajouter
+                    if (motAbsent) {
+                        dicoMots.add(new PaireChaineEntier(currentWord, 0));
+                    }
                 }
             }
         }
-        return dico;
+        return dicoMots;
     }
 
-    public static void calculScores(ArrayList<Depeche> depeches, String categorie,
-            ArrayList<PaireChaineEntier> dictionnaire) {
+    public static void calculScores(ArrayList<Depeche> depeches, String categorie, ArrayList<PaireChaineEntier> dictionnaire) {
+        String currentWord;
+        for (int i = 0; i < depeches.size(); i++) {
+            // Parcours de toutes les dépêches
+            for (int j = 0; j < depeches.get(i).getMots().size(); j++) {
+                // Pour chaque dépêche, parcours de tous ses mots
+                currentWord = depeches.get(i).getMots().get(j); // Variable permettant de référencer le mot concerné par le tour de boucle actuel
+                for (int k = 0; k < dictionnaire.size(); k++) {
+                    // Parcours des éléments du dictionnaire donné en argument pour chercher si le mot actuel (currentWord) est le même qu'un des mots du dictionnaire :
+                    if (dictionnaire.get(k).getChaine().equals(currentWord)) {
+                        // Si c'est le cas, on vérifie si la catégorie est la bonne
+                        if (depeches.get(i).getCategorie().equals(categorie)) {
+                            // Si la catégorie est la bonne, on veut donner un meilleur score à cet élément :
+                            dictionnaire.get(k).setEntier(dictionnaire.get(k).getEntier() + 1);
+                        } else {
+                            // Sinon, c'est l'inverse :
+                            dictionnaire.get(k).setEntier(dictionnaire.get(k).getEntier() - 1);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public static int poidsPourScore(int score) {
         return 0;
     }
 
-    public static void generationLexique(ArrayList<Depeche> depeches, String categorie, String nomFichier) {
-
-    }
+    public static void generationLexique(ArrayList<Depeche> depeches, String categorie, String nomFichier) {}
 
     public static void afficherListeDepeches(ArrayList<Depeche> depeches) {
         for (int i = 0; i < depeches.size(); i++) {
@@ -139,26 +175,56 @@ public class Classification {
         System.out.println("Catégorie avec le score maximal pour la dépêche actuelle : " + UtilitairePaireChaineEntier.chaineMax(scores));
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void testInitDico(ArrayList<Depeche> depeches, String categorieCible, int filtreMinimum) {
+        
+        ArrayList<PaireChaineEntier> dico = initDico(depeches, categorieCible);
+        calculScores(depeches, categorieCible, dico);
+        System.out.println("Liste des mots dont le score est plus grand que " + filtreMinimum + " pour la catégorie " + categorieCible + " :");
+        for (int i = 0; i < dico.size(); i++) {
+            if (dico.get(i).getEntier() > filtreMinimum) {
+                System.out.println(dico.get(i));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
 
         // Chargement des dépêches en mémoire
-        System.out.println("chargement des dépêches");
-        ArrayList<Depeche> depeches = lectureDepeches("./depeches.txt");
-
+        ArrayList<Depeche> depeches = lectureDepeches("depeches.txt");
+        ArrayList<Depeche> depechesTest = lectureDepeches("test.txt");
+        
+        // Création des objets catégories
         Categorie environnement_sciences = new Categorie("ENVIRONNEMENT-SCIENCES");
-        environnement_sciences.initLexique();
         Categorie culture = new Categorie("CULTURE");
-        culture.initLexique();
         Categorie economie = new Categorie("ECONOMIE");
-        economie.initLexique();
         Categorie politique = new Categorie("POLITIQUE");
-        politique.initLexique();
         Categorie sports = new Categorie("SPORTS");
+
+        // Chargement des lexiques en mémoire pour chaque catégorie
+        environnement_sciences.initLexique();
+        culture.initLexique();
+        economie.initLexique();
+        politique.initLexique();
         sports.initLexique();
+
+        // Variable utilitaire qui regroupe toutes les catégories
         ArrayList<Categorie> categories = new ArrayList<>(
-                Arrays.asList(environnement_sciences, culture, economie, politique, sports));
+            Arrays.asList(
+                environnement_sciences,
+                culture,
+                economie,
+                politique,
+                sports
+            )
+        );
 
+        // Ecriture du résultat des classements dans les fichiers :
         classementDepeches(depeches, categories, "classement.txt");
+        classementDepeches(depechesTest, categories, "classementTest.txt");
 
+
+
+        // Test de initDico
+        testInitDico(depeches, "CULTURE", 0);
     }
 }
